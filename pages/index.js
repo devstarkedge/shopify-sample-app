34bf938772919ee07764fba86d3a2505512df0ab
devstarkedge <34094470+devstarkedge@users.noreply.github.com>
2022-03-07 17:54:58 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import { Layout, Page, TextStyle } from '@shopify/polaris';
+
+const Index = () => (
+  <Page>
+  <Layout>
+    <TextStyle variation="positive">
+      Sample app using React and Next.js
+    </TextStyle>
+	</Layout>
+  </Page>
+);
+
+export default Index;
